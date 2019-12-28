2f526495fb9e3f1e64b674baad32158f4fb2edb6
ing <inyoman_gurnitha@yahoo.com>
2019-12-28 23:41:44 +0700

    13. Intro to Blade Template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,26 @@
+@extends('master')
+
+@section('sidebar')
+	@parent
+	<p>Under at parent</p>
+@endsection
+
+@section('component')	
+
 <h1>About page</h1>
 <p>Firstname: <?php echo $fname; ?></p>
 <p>Lastname: <?php echo $lname; ?></p>
+
+<hr>
+
+<p>Firstname: {{ $fname }}</p>
+<p>Lastname: {{ $lname }}</p>
+
+<hr>
+
+@php
+	$name = "ING";
+	echo $name;
+@endphp	
+
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Online News</title>
+</head>
+<body>
+	@section('sidebar')
+	<p>Sidebar</p>
+
+	@show
+
+	<div>
+		@yield('component')
+	</div>
+</body>
+</html>
